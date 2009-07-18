9e412a7b1fbc03bb2d807a188d6611f2222673ef
Jonathan Swartz <swartz@pobox.com>
2009-07-18 09:42:46 -0700

    init

---
+++ lib/Test/Log/Dispatch.pm
@@ -0,0 +1,45 @@
+package Test::Log::Dispatch;
+use 5.006;
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+1;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Test::Log::Dispatch -- Test what you are logging
+
+=head1 SYNOPSIS
+
+    use Test::Log::Dispatch;
+
+=head1 DESCRIPTION
+
+Test::Log::Dispatch provides
+
+=head1 AUTHOR
+
+Jonathan Swartz
+
+=head1 SEE ALSO
+
+L<Some::Module>
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright (C) 2007 Jonathan Swartz.
+
+Test::Log::Dispatch is provided "as is" and without any express or implied warranties, including, without
+limitation, the implied warranties of merchantibility and fitness for a particular
+purpose.
+
+This program is free software; you can redistribute it and/or modify it under the same
+terms as Perl itself.
+
+=cut
---
+++ t/Sanity.t
@@ -0,0 +1,3 @@
+#!perl -w
+use Test::Log::Dispatch::t::Sanity;
+Test::Log::Dispatch::t::Sanity->runtests;
